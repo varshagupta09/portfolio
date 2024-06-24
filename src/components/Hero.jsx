4977cfa109a195a75f7b818620333e68243af7ee
Varsha Gupta <vrsa8896@gmail.com>
2024-06-24 15:50:30 +0530

    portfolio

---
+++ src/components/Hero.jsx
@@ -0,0 +1,34 @@
+import {HERO_CONTENT} from "../constants";
+import profilePic from "../assets/VarshaGuptaProfile.png"
+const Hero = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start">
+            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+              Varsha Gupta
+            </h1>
+
+            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent ">
+Frontend Developer
+            </span>
+
+            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+                {HERO_CONTENT}
+            </p>
+          </div>
+        </div>
+        <div className="'w-full lg:w-1/2 lg:p-8">
+<div className="flex justify-center ">
+    <img src={profilePic} alt="Varsha Gupta" className="rounded-xl"/>
+
+</div>
+      </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import logo from "../assets/kevinRushLogo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6 ">
+      <div className="flex flex-shrink-0 items-center font-medium text-3xl text-white">
+        <h1>VG</h1>
+        {/* <img className="mx-2 w-10" src={logo} alt="" /> */}
+      </div>
+      <div className="m-8 flex  items-center justify-between gap-4 text-2xl" >
+        <FaLinkedin />
+        <FaGithub />
+        <FaInstagram />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
